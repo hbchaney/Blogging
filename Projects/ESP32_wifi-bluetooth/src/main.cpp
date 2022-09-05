70606307b469700abcc7739eb20bef0fc2903563
Harrison Chaney <hbchaney@gmail.com>
2022-09-04 22:14:34 -0400

    update

--- Projects/ESP32_wifi-bluetooth/src/main.cpp
+++ Projects/ESP32_wifi-bluetooth/src/main.cpp
@@ -2,10 +2,20 @@
 #include <WiFi.h>
 #include "Adafruit_NeoPixel.h"
 
+Adafruit_NeoPixel lightbulb {1,2};  
+
+
 void setup() {
   // put your setup code here, to run once:
+  lightbulb.begin();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  lightbulb.setPixelColor(0,125,125,125);
+
+
+
 }
